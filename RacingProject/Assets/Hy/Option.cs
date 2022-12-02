65f47b48a971397cfa738f35a462a3fc5a3cf590
hyhy02 <sss601301@naver.com>
2022-12-02 22:47:51 +0900

    튜토리얼, 메인화면 UI, 카운트다운
    하영- 튜토리얼씬, 메인화면 UI 버튼기능, 게임 시작시 카운트다운, 씬전환

---
+++ RacingProject/Assets/Hy/Option.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Profiling;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Option : MonoBehaviour
+{
+    public GameObject OptionPanel;
+
+    public CountDown cd;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape)) //esc누르면 옵션 
+        {
+            Time.timeScale = 0; //게임 일시정지
+            OptionPanel.SetActive(true);
+        }
+
+        
+    }
+    // Update is called once per frame
+    public void GoOption() //옵션화면
+    {
+            //Time.timeScale = 0; //게임 일시정지
+            OptionPanel.SetActive(true);
+    }
+    public void OutOption()
+    {
+        OptionPanel.SetActive(false);
+    }
+    public void Continue() //게임계속하기
+    {
+        Time.timeScale = 1;
+        OptionPanel.SetActive(false);
+        cd.Timer = 0;
+        cd.Count();
+    }
+
+    public void GameExit() //게임끝내기
+    {
+        Application.Quit();
+    }
+    public void RestartScenesButton() //씬 재시작
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+}
