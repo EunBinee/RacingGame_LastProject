4a4839991ab14675f5c930a3d64ae299154b44f0
KSYsuyun <105999143+KSYsuyun@users.noreply.github.com>
2022-12-07 23:30:16 +0900

    체크포인트 수정, 부스터 추가, 새로운 씬 추가
    수연
    
    자잘자잘 한 것을 했음...

--- RacingProject/Assets/Hy/Script/Boost.cs
+++ RacingProject/Assets/Hy/Script/Boost.cs
@@ -8,6 +8,13 @@ public class Boost : MonoBehaviour
 {
     private GameObject Player;
     private BicycleController ct;
+
+    [SerializeField] private bool isBoost = false;
+
+    [SerializeField] private float time = 0;
+    [SerializeField] private float maxTime = 3;         //5초동안 부스터
+    private float fixTime = 3;      //고정된 시간
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,31 +23,50 @@ public class Boost : MonoBehaviour
     }
 
 
+    private void Update()
+    {
+        if (isBoost)
+        {
+            if (time > maxTime)
+            {
+                Debug.Log("시간 끝");
+                time = 0;
+                maxTime = fixTime;
+                ct.sprint = false;
+                isBoost = false;
+            }
+            else
+            {
+                time += Time.deltaTime;
+                ct.sprint = true;
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Bicycle"))
+        if (other.gameObject.CompareTag("Wheel"))
         {
-            ct.sprint = true;
-            Invoke("delay", 3f);
-            //Debug.Log("sads");
+            if (isBoost)
+            {
+                Debug.Log("시간추가");
+                time = 0;
+            }
+            else
+            {
+                Debug.Log("달린다!");
+                isBoost = true;
+            }
+
+
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("Bicycle"))
+        if (other.gameObject.CompareTag("Wheel"))
         {
-            //ct.sprint = false;
-            //Invoke("delay", 3f);
-            //Debug.Log("sads");
+
         }
     }
 
-    private void delay()
-    {
-        ct.sprint = false;
-        ct.currentSpeed -= 10;
-        CancelInvoke();
-        //Debug.Log("del");
-
-    }
 }
