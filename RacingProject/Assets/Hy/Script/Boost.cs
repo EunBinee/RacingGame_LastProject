f0a783c6be79434ddb59c8bbc09c716f89b6c65a
hyhy02 <sss601301@naver.com>
2022-12-09 22:06:49 +0900

    부스터 수정
    .

--- RacingProject/Assets/Hy/Script/Boost.cs
+++ RacingProject/Assets/Hy/Script/Boost.cs
@@ -4,62 +4,99 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+
 public class Boost : MonoBehaviour
 {
-    private GameObject Player;
+    //private GameObject Player;
     private BicycleController ct;
-    private int countBoost=0;
-    // Start is called before the first frame update
+    public GameObject[] runners;
+    [SerializeField] List<BicycleController> runnerBicycleList;
+
+    [SerializeField] private bool isBoost = false;
+
+    [SerializeField] private float time = 0;
+    [SerializeField] private float maxTime = 3;         //5초동안 부스터
+    private float fixTime = 3;      //고정된 시간
+
+    private List<int> num;
+
     void Start()
     {
-        Player = GameObject.FindGameObjectWithTag("Bicycle");
-        ct = Player.GetComponent<BicycleController>();
+        runners = GameObject.FindGameObjectsWithTag("Bicycle");
+        runnerBicycleList = new List<BicycleController>();
+
+        for (int i = 0; i < runners.Length; i++)
+        {
+            runnerBicycleList.Add(runners[i].GetComponent<BicycleController>());
+        }
+        // runnerBicycleList[0].sprint = true;
+        num = new List<int>();
     }
     void Update()
     {
-        Booster();
+        if (isBoost)
+        {
+            if (time > maxTime)
+            {
+                Debug.Log("시간 끝");
+                time = 0;
+                maxTime = fixTime;
+                //ct.sprint = false;
+                foreach (int i in num)
+                {
+                    runnerBicycleList[i].sprint = false;
+                }
+
+
+                isBoost = false;
+            }
+            else
+            {
+                time += Time.deltaTime;
+                //ct.sprint = true;
+               
+            }
+           
+        }
+        else { num.Clear(); }
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Bicycle"))
+        if (other.gameObject.CompareTag("Wheel"))
         {
-            countBoost += 1;
-            //ct.sprint = true;
-            //Invoke("delay", 3f);
-            //Debug.Log("sads");
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Bicycle"))
-        {
-            //ct.sprint = false;
-            //Invoke("delay", 3f);
-            //Debug.Log("sads");
+            if (isBoost)
+            {
+                for (int i = 0; i < runners.Length; i++)
+                {
+                    if (other.transform.parent.name == runners[i].name)
+                    {
+                        Debug.Log("시간추가");
+                        time = 0;
+
+                        num.Add(i);
+                        runnerBicycleList[i].sprint = true;
+                    }
+
+                }
+            }
+            else
+            {
+                for (int i = 0; i < runners.Length; i++)
+                {
+                    if (other.transform.parent.name == runners[i].name)
+                    {
+                        Debug.Log("달린다!");
+                        isBoost = true;
+
+                        num.Add(i);
+                        runnerBicycleList[i].sprint = true;
+                    }
+                }
+            }
         }
     }
 
-    private void delay()
-    {
-        ct.sprint = false;
-        ct.currentSpeed -= 10;
-        //CancelInvoke();
-        //Debug.Log("del");
-    }
-    private void Booster()
-    {
-        if (countBoost > 0)
-        {
-            //for()
-            //{
-            //    ct.sprint = true;
-            //    //Invoke("delay", 3f);
-            //}
-        }
-        else
-        {
-            //CancelInvoke();
-        }
-    }
+
 }
