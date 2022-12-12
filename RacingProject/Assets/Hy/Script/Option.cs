98d5773efbe99002fdc68388851c290f7f8c7041
hyhy02 <sss601301@naver.com>
2022-12-12 22:24:58 +0900

    옵션 제일 위로
    .

--- RacingProject/Assets/Hy/Script/Option.cs
+++ RacingProject/Assets/Hy/Script/Option.cs
@@ -12,6 +12,7 @@ public class Option : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        OptionPanel.transform.SetAsLastSibling();
     }
     private void Update()
     {
